adf63d3790df35858c270145adcf3077795d3b42
Shiyang Wang <your_email@example.com>
2016-12-21 16:56:39 +0800

    add zsh completions support for s2i command

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	utilglog "github.com/openshift/source-to-image/pkg/util/glog"
+	"github.com/spf13/cobra"
 )
 
 // Common S2I errors
@@ -276,4 +277,9 @@ func CheckError(err error) {
 		glog.Errorf("An error occurred: %v", err)
 		os.Exit(1)
 	}
+}
+
+func UsageError(cmd *cobra.Command, format string, args ...interface{}) error {
+	msg := fmt.Sprintf(format, args...)
+	return fmt.Errorf("%s\nSee '%s -h' for help and examples.", msg, cmd.CommandPath())
 }
